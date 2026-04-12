be37b95f259d614e27be16c6841a4907fbf3918c
NotVarunK <varunkolambekar@gmail.com>
2026-04-12 18:06:32 +0530

    Update server.py

--- server.py
+++ server.py
@@ -25,4 +25,4 @@ def chat():
 
 if __name__ == "__main__":
     print("MediBot server running at http://localhost:5000")
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=False)
